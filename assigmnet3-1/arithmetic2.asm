fffc6ae8809faf5848ef343525344d3a281fcb10
Monique Murphy <murphy.moniqued@gmail.com>
2026-07-06 03:20:28 +0000

    Equation 2 with result of 50

---
+++ assigmnet3-1/arithmetic2.asm
@@ -0,0 +1,21 @@
+section .text
+    global _start
+
+_start:
+    mov eax, [var1]
+    add eax, [var2]
+    add eax, [var3]
+    add eax, [var4]
+    mov [result], eax
+
+    mov eax, 1
+    int 0x80
+
+segment .bss
+    result resd 1
+
+section .data
+    var1 DD 15
+    var2 DD 20
+    var3 DD 5
+    var4 DD 10
