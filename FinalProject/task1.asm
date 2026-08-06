a7f2d72ef160856732eeb9776a44764f7f04d992
Monique Murphy <murphy.moniqued@gmail.com>
2026-08-06 01:33:28 +0000

    final project task 1 xor encryption

---
+++ FinalProject/task1.asm
@@ -0,0 +1,138 @@
+section .text
+    global _start
+
+_start:
+    ;read message
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, message
+    mov edx, 10
+    int 0x80
+
+    ; read key 
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, key
+    mov edx, 10
+    int 0x80
+
+    ; file open 
+    mov ecx, 0711o
+    mov ebx, filename
+    mov eax, 8
+    int 0x80
+    mov [fd_out], eax
+
+    ; write Plain text:
+    mov eax, 4 
+    mov ebx, [fd_out]
+    mov ecx, label1
+    mov edx, len_label1
+    int 0x80 
+
+    ; write message
+    mov eax, 4
+    mov ebx, [fd_out]
+    mov ecx, message
+    mov edx, 10
+    int 0x80
+
+    ; write Key:
+    mov eax, 4
+    mov ebx, [fd_out]
+    mov ecx, label2
+    mov edx, len_label2
+    int 0x80
+
+    ; write key
+    mov eax, 4
+    mov ebx, [fd_out]
+    mov ecx, key
+    mov edx, 10
+    int 0x80
+
+    ; Encryption loop 
+    mov eax, 10
+    mov ecx, message
+    mov edx, key
+    mov esi, encrypted
+
+    encryption_loop:    
+        mov bl, [ecx]
+        xor bl, [edx]
+        inc ecx
+        inc edx
+        mov [esi], bl
+        inc esi
+        dec eax
+        jnz encryption_loop
+
+    ; Decryption loop
+    mov eax, 10
+    mov ecx, encrypted
+    mov edx, key
+    mov esi, decrypted
+
+    decryption_loop:
+        mov bl, [ecx]
+        xor bl, [edx]
+        inc ecx
+        inc edx
+        mov [esi], bl
+        inc esi
+        dec eax
+        jnz decryption_loop
+
+    ; write Encrypted Text:
+    mov eax, 4
+    mov ebx, [fd_out]
+    mov ecx, label3
+    mov edx, len_label3
+    int 0x80
+
+    ; write encrypted
+    mov eax, 4
+    mov ebx, [fd_out]
+    mov ecx, encrypted
+    mov edx, 10
+    int 0x80
+
+    ; write Decrypted text:
+    mov eax, 4
+    mov ebx, [fd_out]
+    mov ecx, label4
+    mov edx, len_label4
+    int 0x80
+
+    ; write decrypted
+    mov eax, 4
+    mov ebx, [fd_out]
+    mov ecx, decrypted
+    mov edx, 10
+    int 0x80
+
+    ; file close
+    mov eax, 6
+    mov ebx, [fd_out]
+    int 0x80
+
+    mov eax, 1
+    int 0x80
+
+section .bss
+    fd_out resd 1
+    message resb 10
+    key resb 10
+    encrypted resb 10
+    decrypted resb 10
+
+section .data
+    filename db 'output.txt', 0h
+    label1 db 'Plain text: ', 0h
+    len_label1 equ $ - label1 - 1
+    label2 db 'Key: ', 0h
+    len_label2 equ $ - label2 - 1
+    label3 db 'Encrypted text: ', 0h
+    len_label3 equ $ - label3 - 1
+    label4 db 'Decrypted text: ', 0h
+    len_label4 equ $ - label4 - 1
